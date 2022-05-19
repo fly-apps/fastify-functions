afe42a12eb9d63f6f75114896bafb707e48fe9c8
Joshua Sierles <joshua@fly.io>
2022-05-19 22:59:28 +0200

    ludicious speed

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const fastify = require('fastify')({ logger: true })
 
 fastify.get('/', async (request, reply) => {
-  return { hello: 'world' }
+  return { speed: 'ludicrous' }
 })
 
 const start = async () => {
